43c6656820eeafca65e10fe9baea599b094a8eff
CHE6yp <che6yp@gmail.com>
2024-10-06 00:00:00 +0300

    Init

---
+++ hevsgay.sh
@@ -0,0 +1,478 @@
+#! /bin/bash
+
+# # Х
+# echo
+# echo $(date -d '2024/10/06')
+# echo $(date -d '2024/10/06 +1 day')
+# echo $(date -d '2024/10/06 +5 day')
+# echo $(date -d '2024/10/06 +6 day')
+# echo $(date -d '2024/10/06 +8 day')
+# echo $(date -d '2024/10/06 +9 day')
+# echo $(date -d '2024/10/06 +11 day')
+# echo $(date -d '2024/10/06 +12 day')
+# echo $(date -d '2024/10/06 +16 day')
+# echo $(date -d '2024/10/06 +17 day')
+# echo $(date -d '2024/10/06 +18 day')
+# echo $(date -d '2024/10/06 +22 day')
+# echo $(date -d '2024/10/06 +23 day')
+# echo $(date -d '2024/10/06 +25 day')
+# echo $(date -d '2024/10/06 +26 day')
+# echo $(date -d '2024/10/06 +28 day')
+# echo $(date -d '2024/10/06 +29 day')
+# echo $(date -d '2024/10/06 +32 day')
+# echo $(date -d '2024/10/06 +33 day')
+# echo $(date -d '2024/10/06 +34 day')
+
+# # Sun Oct 6 00:00:00 MSK 2024
+# # Mon Oct 7 00:00:00 MSK 2024
+# # Fri Oct 11 00:00:00 MSK 2024
+# # Sat Oct 12 00:00:00 MSK 2024
+# # Mon Oct 14 00:00:00 MSK 2024
+# # Tue Oct 15 00:00:00 MSK 2024
+# # Thu Oct 17 00:00:00 MSK 2024
+# # Fri Oct 18 00:00:00 MSK 2024
+# # Tue Oct 22 00:00:00 MSK 2024
+# # Wed Oct 23 00:00:00 MSK 2024
+# # Thu Oct 24 00:00:00 MSK 2024
+# # Mon Oct 28 00:00:00 MSK 2024
+# # Tue Oct 29 00:00:00 MSK 2024
+# # Thu Oct 31 00:00:00 MSK 2024
+# # Fri Nov 1 00:00:00 MSK 2024
+# # Sun Nov 3 00:00:00 MSK 2024
+# # Mon Nov 4 00:00:00 MSK 2024
+# # Thu Nov 7 00:00:00 MSK 2024
+# # Fri Nov 8 00:00:00 MSK 2024
+# # Sat Nov 9 00:00:00 MSK 2024
+
+# # Е
+# echo
+# echo $(date -d '2024/11/17')
+# echo $(date -d '2024/11/17 +1 day')
+# echo $(date -d '2024/11/17 +2 day')
+# echo $(date -d '2024/11/17 +3 day')
+# echo $(date -d '2024/11/17 +4 day')
+# echo $(date -d '2024/11/17 +5 day')
+# echo $(date -d '2024/11/17 +6 day')
+# echo $(date -d '2024/11/17 +7 day')
+# echo $(date -d '2024/11/17 +8 day')
+# echo $(date -d '2024/11/17 +9 day')
+# echo $(date -d '2024/11/17 +10 day')
+# echo $(date -d '2024/11/17 +11 day')
+# echo $(date -d '2024/11/17 +12 day')
+# echo $(date -d '2024/11/17 +13 day')
+
+# echo $(date -d '2024/11/17 +14 day')
+# echo $(date -d '2024/11/17 +16 day')
+# echo $(date -d '2024/11/17 +20 day')
+
+# echo $(date -d '2024/11/17 +21 day')
+# echo $(date -d '2024/11/17 +23 day')
+# echo $(date -d '2024/11/17 +27 day')
+
+# echo $(date -d '2024/11/17 +28 day')
+# echo $(date -d '2024/11/17 +30 day')
+# echo $(date -d '2024/11/17 +34 day')
+
+
+# # Sun Nov 17 00:00:00 MSK 2024
+# # Mon Nov 18 00:00:00 MSK 2024
+# # Tue Nov 19 00:00:00 MSK 2024
+# # Wed Nov 20 00:00:00 MSK 2024
+# # Thu Nov 21 00:00:00 MSK 2024
+# # Fri Nov 22 00:00:00 MSK 2024
+# # Sat Nov 23 00:00:00 MSK 2024
+# # Sun Nov 24 00:00:00 MSK 2024
+# # Mon Nov 25 00:00:00 MSK 2024
+# # Tue Nov 26 00:00:00 MSK 2024
+# # Wed Nov 27 00:00:00 MSK 2024
+# # Thu Nov 28 00:00:00 MSK 2024
+# # Fri Nov 29 00:00:00 MSK 2024
+# # Sat Nov 30 00:00:00 MSK 2024
+# # Sun Dec 1 00:00:00 MSK 2024
+# # Tue Dec 3 00:00:00 MSK 2024
+# # Sat Dec 7 00:00:00 MSK 2024
+# # Sun Dec 8 00:00:00 MSK 2024
+# # Tue Dec 10 00:00:00 MSK 2024
+# # Sat Dec 14 00:00:00 MSK 2024
+# # Sun Dec 15 00:00:00 MSK 2024
+# # Tue Dec 17 00:00:00 MSK 2024
+# # Sat Dec 21 00:00:00 MSK 2024
+
+# # В
+# echo
+# echo $(date -d '2024/12/29')
+# echo $(date -d '2024/12/29 +1 day')
+# echo $(date -d '2024/12/29 +2 day')
+# echo $(date -d '2024/12/29 +3 day')
+# echo $(date -d '2024/12/29 +4 day')
+# echo $(date -d '2024/12/29 +5 day')
+# echo $(date -d '2024/12/29 +6 day')
+# echo $(date -d '2024/12/29 +7 day')
+# echo $(date -d '2024/12/29 +8 day')
+# echo $(date -d '2024/12/29 +9 day')
+# echo $(date -d '2024/12/29 +10 day')
+# echo $(date -d '2024/12/29 +11 day')
+# echo $(date -d '2024/12/29 +12 day')
+# echo $(date -d '2024/12/29 +13 day')
+
+# echo $(date -d '2024/12/29 +14 day')
+# echo $(date -d '2024/12/29 +16 day')
+# echo $(date -d '2024/12/29 +20 day')
+
+# echo $(date -d '2024/12/29 +21 day')
+# echo $(date -d '2024/12/29 +23 day')
+# echo $(date -d '2024/12/29 +27 day')
+
+# echo $(date -d '2024/12/29 +28 day')
+# echo $(date -d '2024/12/29 +29 day')
+# echo $(date -d '2024/12/29 +30 day')
+# echo $(date -d '2024/12/29 +31 day')
+# echo $(date -d '2024/12/29 +32 day')
+# echo $(date -d '2024/12/29 +33 day')
+# echo $(date -d '2024/12/29 +34 day')
+
+# # Sun Dec 29 00:00:00 MSK 2024
+# # Mon Dec 30 00:00:00 MSK 2024
+# # Tue Dec 31 00:00:00 MSK 2024
+# # Wed Jan 1 00:00:00 MSK 2025
+# # Thu Jan 2 00:00:00 MSK 2025
+# # Fri Jan 3 00:00:00 MSK 2025
+# # Sat Jan 4 00:00:00 MSK 2025
+# # Sun Jan 5 00:00:00 MSK 2025
+# # Mon Jan 6 00:00:00 MSK 2025
+# # Tue Jan 7 00:00:00 MSK 2025
+# # Wed Jan 8 00:00:00 MSK 2025
+# # Thu Jan 9 00:00:00 MSK 2025
+# # Fri Jan 10 00:00:00 MSK 2025
+# # Sat Jan 11 00:00:00 MSK 2025
+# # Sun Jan 12 00:00:00 MSK 2025
+# # Tue Jan 14 00:00:00 MSK 2025
+# # Sat Jan 18 00:00:00 MSK 2025
+# # Sun Jan 19 00:00:00 MSK 2025
+# # Tue Jan 21 00:00:00 MSK 2025
+# # Sat Jan 25 00:00:00 MSK 2025
+# # Sun Jan 26 00:00:00 MSK 2025
+# # Mon Jan 27 00:00:00 MSK 2025
+# # Tue Jan 28 00:00:00 MSK 2025
+# # Wed Jan 29 00:00:00 MSK 2025
+# # Thu Jan 30 00:00:00 MSK 2025
+# # Fri Jan 31 00:00:00 MSK 2025
+# # Sat Feb 1 00:00:00 MSK 2025
+
+
+# # С
+# echo
+# echo $(date -d '2025/02/09')
+# echo $(date -d '2025/02/09 +1 day')
+# echo $(date -d '2025/02/09 +2 day')
+# echo $(date -d '2025/02/09 +3 day')
+# echo $(date -d '2025/02/09 +4 day')
+# echo $(date -d '2025/02/09 +5 day')
+# echo $(date -d '2025/02/09 +6 day')
+# echo $(date -d '2025/02/09 +7 day')
+# echo $(date -d '2025/02/09 +8 day')
+# echo $(date -d '2025/02/09 +9 day')
+# echo $(date -d '2025/02/09 +10 day')
+# echo $(date -d '2025/02/09 +11 day')
+# echo $(date -d '2025/02/09 +12 day')
+# echo $(date -d '2025/02/09 +13 day')
+
+# echo $(date -d '2025/02/09 +14 day')
+# echo $(date -d '2025/02/09 +20 day')
+
+# echo $(date -d '2025/02/09 +21 day')
+# echo $(date -d '2025/02/09 +27 day')
+
+# echo $(date -d '2025/02/09 +28 day')
+# echo $(date -d '2025/02/09 +34 day')
+
+# # О
+# echo
+# echo $(date -d '2025/03/23')
+# echo $(date -d '2025/03/23 +1 day')
+# echo $(date -d '2025/03/23 +2 day')
+# echo $(date -d '2025/03/23 +3 day')
+# echo $(date -d '2025/03/23 +4 day')
+# echo $(date -d '2025/03/23 +5 day')
+# echo $(date -d '2025/03/23 +6 day')
+# echo $(date -d '2025/03/23 +7 day')
+# echo $(date -d '2025/03/23 +8 day')
+# echo $(date -d '2025/03/23 +9 day')
+# echo $(date -d '2025/03/23 +10 day')
+# echo $(date -d '2025/03/23 +11 day')
+# echo $(date -d '2025/03/23 +12 day')
+# echo $(date -d '2025/03/23 +13 day')
+
+# echo $(date -d '2025/03/23 +14 day')
+# echo $(date -d '2025/03/23 +20 day')
+
+# echo $(date -d '2025/03/23 +21 day')
+# echo $(date -d '2025/03/23 +27 day')
+
+# echo $(date -d '2025/03/23 +28 day')
+# echo $(date -d '2025/03/23 +29 day')
+# echo $(date -d '2025/03/23 +30 day')
+# echo $(date -d '2025/03/23 +31 day')
+# echo $(date -d '2025/03/23 +32 day')
+# echo $(date -d '2025/03/23 +33 day')
+# echo $(date -d '2025/03/23 +34 day')
+# # Н
+# echo
+# echo $(date -d '2025/05/04')
+# echo $(date -d '2025/05/04 +1 day')
+# echo $(date -d '2025/05/04 +2 day')
+# echo $(date -d '2025/05/04 +3 day')
+# echo $(date -d '2025/05/04 +4 day')
+# echo $(date -d '2025/05/04 +5 day')
+# echo $(date -d '2025/05/04 +6 day')
+# echo $(date -d '2025/05/04 +7 day')
+# echo $(date -d '2025/05/04 +8 day')
+# echo $(date -d '2025/05/04 +9 day')
+# echo $(date -d '2025/05/04 +10 day')
+# echo $(date -d '2025/05/04 +11 day')
+# echo $(date -d '2025/05/04 +12 day')
+# echo $(date -d '2025/05/04 +13 day')
+
+# echo $(date -d '2025/05/04 +16 day')
+
+# echo $(date -d '2025/05/04 +23 day')
+
+# echo $(date -d '2025/05/04 +28 day')
+# echo $(date -d '2025/05/04 +29 day')
+# echo $(date -d '2025/05/04 +30 day')
+# echo $(date -d '2025/05/04 +31 day')
+# echo $(date -d '2025/05/04 +32 day')
+# echo $(date -d '2025/05/04 +33 day')
+# echo $(date -d '2025/05/04 +34 day')
+
+# # г
+# echo $(date -d '2025/06/23')
+# echo $(date -d '2025/06/23 +1 day')
+# echo $(date -d '2025/06/23 +2 day')
+# echo $(date -d '2025/06/23 +3 day')
+# echo $(date -d '2025/06/23 +4 day')
+# echo $(date -d '2025/06/23 +7 day')
+# echo $(date -d '2025/06/23 +14 day')
+# echo $(date -d '2025/06/23 +21 day')
+# # е
+# echo $(date -d '2025/07/28')
+# echo $(date -d '2025/07/28 +1 day')
+# echo $(date -d '2025/07/28 +2 day')
+# echo $(date -d '2025/07/28 +3 day')
+# echo $(date -d '2025/07/28 +4 day')
+# echo $(date -d '2025/07/28 +7 day')
+# echo $(date -d '2025/07/28 +9 day')
+# echo $(date -d '2025/07/28 +11 day')
+# echo $(date -d '2025/07/28 +14 day')
+# echo $(date -d '2025/07/28 +16 day')
+# echo $(date -d '2025/07/28 +18 day')
+# echo $(date -d '2025/07/28 +21 day')
+# echo $(date -d '2025/07/28 +25 day')
+
+# # й
+# echo $(date -d '2025/09/01')
+# echo $(date -d '2025/09/01 +1 day')
+# echo $(date -d '2025/09/01 +2 day')
+# echo $(date -d '2025/09/01 +3 day')
+# echo $(date -d '2025/09/01 +4 day')
+# echo $(date -d '2025/09/01 +10 day')
+# echo $(date -d '2025/09/01 +13 day')
+# echo $(date -d '2025/09/01 +16 day')
+# echo $(date -d '2025/09/01 +20 day')
+# echo $(date -d '2025/09/01 +21 day')
+# echo $(date -d '2025/09/01 +22 day')
+# echo $(date -d '2025/09/01 +23 day')
+# echo $(date -d '2025/09/01 +24 day')
+
+dates=("Mon Oct 7 00:00:00 2024 +0300" 
+"Fri Oct 11 00:00:00 2024 +0300" 
+"Sat Oct 12 00:00:00 2024 +0300" 
+"Mon Oct 14 00:00:00 2024 +0300" 
+"Tue Oct 15 00:00:00 2024 +0300" 
+"Thu Oct 17 00:00:00 2024 +0300" 
+"Fri Oct 18 00:00:00 2024 +0300" 
+"Tue Oct 22 00:00:00 2024 +0300" 
+"Wed Oct 23 00:00:00 2024 +0300" 
+"Thu Oct 24 00:00:00 2024 +0300" 
+"Mon Oct 28 00:00:00 2024 +0300" 
+"Tue Oct 29 00:00:00 2024 +0300" 
+"Thu Oct 31 00:00:00 2024 +0300" 
+"Fri Nov 1 00:00:00 2024 +0300" 
+"Sun Nov 3 00:00:00 2024 +0300" 
+"Mon Nov 4 00:00:00 2024 +0300" 
+"Thu Nov 7 00:00:00 2024 +0300" 
+"Fri Nov 8 00:00:00 2024 +0300" 
+"Sat Nov 9 00:00:00 2024 +0300" 
+"Sun Nov 17 00:00:00 2024 +0300" 
+"Mon Nov 18 00:00:00 2024 +0300" 
+"Tue Nov 19 00:00:00 2024 +0300" 
+"Wed Nov 20 00:00:00 2024 +0300" 
+"Thu Nov 21 00:00:00 2024 +0300" 
+"Fri Nov 22 00:00:00 2024 +0300" 
+"Sat Nov 23 00:00:00 2024 +0300" 
+"Sun Nov 24 00:00:00 2024 +0300" 
+"Mon Nov 25 00:00:00 2024 +0300" 
+"Tue Nov 26 00:00:00 2024 +0300" 
+"Wed Nov 27 00:00:00 2024 +0300" 
+"Thu Nov 28 00:00:00 2024 +0300" 
+"Fri Nov 29 00:00:00 2024 +0300" 
+"Sat Nov 30 00:00:00 2024 +0300" 
+"Sun Dec 1 00:00:00 2024 +0300" 
+"Tue Dec 3 00:00:00 2024 +0300" 
+"Sat Dec 7 00:00:00 2024 +0300" 
+"Sun Dec 8 00:00:00 2024 +0300" 
+"Tue Dec 10 00:00:00 2024 +0300" 
+"Sat Dec 14 00:00:00 2024 +0300" 
+"Sun Dec 15 00:00:00 2024 +0300" 
+"Tue Dec 17 00:00:00 2024 +0300" 
+"Sat Dec 21 00:00:00 2024 +0300" 
+"Sun Dec 29 00:00:00 2024 +0300" 
+"Mon Dec 30 00:00:00 2024 +0300" 
+"Tue Dec 31 00:00:00 2024 +0300" 
+"Wed Jan 1 00:00:00 2025 +0300" 
+"Thu Jan 2 00:00:00 2025 +0300" 
+"Fri Jan 3 00:00:00 2025 +0300" 
+"Sat Jan 4 00:00:00 2025 +0300" 
+"Sun Jan 5 00:00:00 2025 +0300" 
+"Mon Jan 6 00:00:00 2025 +0300" 
+"Tue Jan 7 00:00:00 2025 +0300" 
+"Wed Jan 8 00:00:00 2025 +0300" 
+"Thu Jan 9 00:00:00 2025 +0300" 
+"Fri Jan 10 00:00:00 2025 +0300" 
+"Sat Jan 11 00:00:00 2025 +0300" 
+"Sun Jan 12 00:00:00 2025 +0300" 
+"Tue Jan 14 00:00:00 2025 +0300" 
+"Sat Jan 18 00:00:00 2025 +0300" 
+"Sun Jan 19 00:00:00 2025 +0300" 
+"Tue Jan 21 00:00:00 2025 +0300" 
+"Sat Jan 25 00:00:00 2025 +0300" 
+"Sun Jan 26 00:00:00 2025 +0300" 
+"Mon Jan 27 00:00:00 2025 +0300" 
+"Tue Jan 28 00:00:00 2025 +0300" 
+"Wed Jan 29 00:00:00 2025 +0300" 
+"Thu Jan 30 00:00:00 2025 +0300" 
+"Fri Jan 31 00:00:00 2025 +0300" 
+"Sat Feb 1 00:00:00 2025 +0300" 
+"Sun Feb 9 00:00:00 2025 +0300" 
+"Mon Feb 10 00:00:00 2025 +0300" 
+"Tue Feb 11 00:00:00 2025 +0300" 
+"Wed Feb 12 00:00:00 2025 +0300" 
+"Thu Feb 13 00:00:00 2025 +0300" 
+"Fri Feb 14 00:00:00 2025 +0300" 
+"Sat Feb 15 00:00:00 2025 +0300" 
+"Sun Feb 16 00:00:00 2025 +0300" 
+"Mon Feb 17 00:00:00 2025 +0300" 
+"Tue Feb 18 00:00:00 2025 +0300" 
+"Wed Feb 19 00:00:00 2025 +0300" 
+"Thu Feb 20 00:00:00 2025 +0300" 
+"Fri Feb 21 00:00:00 2025 +0300" 
+"Sat Feb 22 00:00:00 2025 +0300" 
+"Sun Feb 23 00:00:00 2025 +0300" 
+"Sat Mar 1 00:00:00 2025 +0300" 
+"Sun Mar 2 00:00:00 2025 +0300" 
+"Sat Mar 8 00:00:00 2025 +0300" 
+"Sun Mar 9 00:00:00 2025 +0300" 
+"Sat Mar 15 00:00:00 2025 +0300" 
+"Sun Mar 23 00:00:00 2025 +0300" 
+"Mon Mar 24 00:00:00 2025 +0300" 
+"Tue Mar 25 00:00:00 2025 +0300" 
+"Wed Mar 26 00:00:00 2025 +0300" 
+"Thu Mar 27 00:00:00 2025 +0300" 
+"Fri Mar 28 00:00:00 2025 +0300" 
+"Sat Mar 29 00:00:00 2025 +0300" 
+"Sun Mar 30 00:00:00 2025 +0300" 
+"Mon Mar 31 00:00:00 2025 +0300" 
+"Tue Apr 1 00:00:00 2025 +0300" 
+"Wed Apr 2 00:00:00 2025 +0300" 
+"Thu Apr 3 00:00:00 2025 +0300" 
+"Fri Apr 4 00:00:00 2025 +0300" 
+"Sat Apr 5 00:00:00 2025 +0300" 
+"Sun Apr 6 00:00:00 2025 +0300" 
+"Sat Apr 12 00:00:00 2025 +0300" 
+"Sun Apr 13 00:00:00 2025 +0300" 
+"Sat Apr 19 00:00:00 2025 +0300" 
+"Sun Apr 20 00:00:00 2025 +0300" 
+"Mon Apr 21 00:00:00 2025 +0300" 
+"Tue Apr 22 00:00:00 2025 +0300" 
+"Wed Apr 23 00:00:00 2025 +0300" 
+"Thu Apr 24 00:00:00 2025 +0300" 
+"Fri Apr 25 00:00:00 2025 +0300" 
+"Sat Apr 26 00:00:00 2025 +0300" 
+"Sun May 4 00:00:00 2025 +0300" 
+"Mon May 5 00:00:00 2025 +0300" 
+"Tue May 6 00:00:00 2025 +0300" 
+"Wed May 7 00:00:00 2025 +0300" 
+"Thu May 8 00:00:00 2025 +0300" 
+"Fri May 9 00:00:00 2025 +0300" 
+"Sat May 10 00:00:00 2025 +0300" 
+"Sun May 11 00:00:00 2025 +0300" 
+"Mon May 12 00:00:00 2025 +0300" 
+"Tue May 13 00:00:00 2025 +0300" 
+"Wed May 14 00:00:00 2025 +0300" 
+"Thu May 15 00:00:00 2025 +0300" 
+"Fri May 16 00:00:00 2025 +0300" 
+"Sat May 17 00:00:00 2025 +0300" 
+"Tue May 20 00:00:00 2025 +0300" 
+"Tue May 27 00:00:00 2025 +0300" 
+"Sun Jun 1 00:00:00 2025 +0300" 
+"Mon Jun 2 00:00:00 2025 +0300" 
+"Tue Jun 3 00:00:00 2025 +0300" 
+"Wed Jun 4 00:00:00 2025 +0300" 
+"Thu Jun 5 00:00:00 2025 +0300" 
+"Fri Jun 6 00:00:00 2025 +0300" 
+"Sat Jun 7 00:00:00 2025 +0300" 
+"Mon Jun 23 00:00:00 2025 +0300" 
+"Tue Jun 24 00:00:00 2025 +0300" 
+"Wed Jun 25 00:00:00 2025 +0300" 
+"Thu Jun 26 00:00:00 2025 +0300" 
+"Fri Jun 27 00:00:00 2025 +0300" 
+"Mon Jun 30 00:00:00 2025 +0300" 
+"Mon Jul 7 00:00:00 2025 +0300" 
+"Mon Jul 14 00:00:00 2025 +0300" 
+"Mon Jul 28 00:00:00 2025 +0300" 
+"Tue Jul 29 00:00:00 2025 +0300" 
+"Wed Jul 30 00:00:00 2025 +0300" 
+"Thu Jul 31 00:00:00 2025 +0300" 
+"Fri Aug 1 00:00:00 2025 +0300" 
+"Mon Aug 4 00:00:00 2025 +0300" 
+"Wed Aug 6 00:00:00 2025 +0300" 
+"Fri Aug 8 00:00:00 2025 +0300" 
+"Mon Aug 11 00:00:00 2025 +0300" 
+"Wed Aug 13 00:00:00 2025 +0300" 
+"Fri Aug 15 00:00:00 2025 +0300" 
+"Mon Aug 18 00:00:00 2025 +0300" 
+"Fri Aug 22 00:00:00 2025 +0300" 
+"Mon Sep 1 00:00:00 2025 +0300" 
+"Tue Sep 2 00:00:00 2025 +0300" 
+"Wed Sep 3 00:00:00 2025 +0300" 
+"Thu Sep 4 00:00:00 2025 +0300" 
+"Fri Sep 5 00:00:00 2025 +0300" 
+"Thu Sep 11 00:00:00 2025 +0300" 
+"Sun Sep 14 00:00:00 2025 +0300" 
+"Wed Sep 17 00:00:00 2025 +0300" 
+"Mon Sep 22 00:00:00 2025 +0300" 
+"Tue Sep 23 00:00:00 2025 +0300" 
+"Wed Sep 24 00:00:00 2025 +0300" 
+"Thu Sep 25 00:00:00 2025 +0300"
+"Fri Sep 26 00:00:00 2025 +0300" 
+)
+
+for i in "${dates[@]}"
+do
+	echo "$i"
+	echo "gay " >> hevsgaytext
+	git add hevsgaytext
+	git commit -m "$i" --date "$i"
+	echo "gay " >> hevsgaytext
+	git add hevsgaytext
+	git commit -m "$i" --date "$i"
+	echo "gay " >> hevsgaytext
+	git add hevsgaytext
+	git commit -m "$i" --date "$i"
+	echo "gay " >> hevsgaytext
+	git add hevsgaytext
+	git commit -m "$i" --date "$i"
+	echo "gay " >> hevsgaytext
+	git add hevsgaytext
+	git commit -m "$i" --date "$i"
+done
